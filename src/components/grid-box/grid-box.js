2ab50d251972291a9eefbb57dabfa052459ab49f
Andreas Lillje <al225wg@student.lnu.se>
2022-10-04 10:37:18 +0200

    Add divs dynamically

---
+++ src/components/grid-box/grid-box.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import './grid-box.css'
+/**
+ * GridBox Component.
+ * Displays a grid box.
+ *
+ * @returns {React.ReactElement} - GridBox Component.
+ */
+ class GridContainer extends React.Component {
+
+    render() {
+        return (<div>
+
+        </div>
+          )
+    }
+  }
+
+export default GridContainer;
+
--- src/components/grid-displayer/grid-displayer.js
+++ src/components/grid-displayer/grid-displayer.js
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { useState, useEffect } from "react"
-import { gridlify } from '/node_modules/gridlify/lib/index.js'
-import './grid-displayer.css'
+import GridBox from "../grid-box/grid-box";
+import { useState, useEffect } from "react";
+import { gridlify } from "/node_modules/gridlify/lib/index.js";
+import "./grid-displayer.css";
 
 /**
  * GridBox Component.
@@ -9,34 +10,59 @@ import './grid-displayer.css'
  *
  * @returns {React.ReactElement} - GridBox Component.
  */
- class GridDisplayer extends React.Component {
-
+class GridDisplayer extends React.Component {
   constructor(props) {
-    super(props)
-    this.gridRows = props.gridRows || ['200px', '200px', '200px', '200px', '200px']
-    this.gridColumns = props.gridColumns|| ['1fr', '1fr', '1fr', '1fr', '1fr']
-    this.gridRowGap = props.gridRowGap || '0px'
-    this.gridColumnGap = props.gridColumnGap || '0px'
-    this.grid = { gridRows: this.gridRows, gridColumns: this.gridColumns, gridRowGap: this.gridRowGap, gridColumnGap: this.gridColumnGap }
+    super(props);
+    this.gridRows = props.gridRows || [
+      "200px",
+      "200px",
+      "200px",
+      "200px",
+      "200px",
+    ];
+    this.gridColumns = props.gridColumns || ["1fr", "1fr", "1fr", "1fr", "1fr"];
+    this.gridRowGap = props.gridRowGap || "0px";
+    this.gridColumnGap = props.gridColumnGap || "0px";
+    this.grid = {
+      gridRows: this.gridRows,
+      gridColumns: this.gridColumns,
+      gridRowGap: this.gridRowGap,
+      gridColumnGap: this.gridColumnGap,
+    };
+    this.currentDivNumber = 0;
   }
 
-  // componentDidMount() {
-  //   gridlify.setGrid(this.grid, '.gridDisplayerContainer')
-
-  // }
-
-  // componentDidUpdate() {
-  //   gridlify.setGrid(this.grid, '.gridDisplayerContainer')
-  // }
-
-    render() {
-        return (
-            <div className="gridDisplayerContainer">
-                
-            </div>
-          )
-    }
+  setNumberOfChildComponents() {
+    this.numberOfChildComponents =
+      this.gridRows.length * this.gridColumns.length;
   }
+
+  incrementDivNumber () {
+    this.currentDivNumber++;
+  }
+
+  getCurrentDivNumber () {
+    this.incrementDivNumber();
+    return this.currentDivNumber
+  }
+  componentDidMount() {
+    // gridlify.setGrid(this.grid, '.gridDisplayerContainer')
+    this.setNumberOfChildComponents();
+    console.log(this.numberOfChildComponents);
+  }
+
+
+  render() {
+    return (
+      <div className="gridDisplayerContainer">
+        {this.gridRows.map(() => {
+          return this.gridColumns.map(() => {
+            return <div key={this.getCurrentDivNumber} className={`div${this.getCurrentDivNumber()}`}></div>;
+          });
+        })}{" "}
+      </div>
+    );
+  }
+}
 
 export default GridDisplayer;
-
