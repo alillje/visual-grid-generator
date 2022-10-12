da60d4ad0652cbf143717bec212bb04620dc15d4
Andreas Lillje <al225wg@student.lnu.se>
2022-10-12 12:37:05 +0200

    Add validator method to validate if parseable to integer

--- src/classes/validator.js
+++ src/classes/validator.js
@@ -19,4 +19,14 @@ export class Validator {
   endIsLessThanStartValue (startPosistion, endPosition) {
     return startPosistion > endPosition
   }
+
+  /**
+   * Validates if a start value is bigger than an end value.
+   *
+   * @param {number} gridProperty - A property in as CSS grid layout.
+   * @returns {boolean} - True if the argument is a parseble value, otherwise false.
+   */
+  isNotParseableToNumber (gridProperty) {
+    return (typeof gridProperty === 'number') && !Number.isNaN(gridProperty) && Number.isInteger(gridProperty)
+  }
 }
