de69a5f790a34fdee6f3aeabb9dea9753eec4b8a
Andreas Lillje <al225wg@student.lnu.se>
2022-11-01 10:52:42 +0100

    Reduce number of colors

--- src/classes/enum/css-colors.js
+++ src/classes/enum/css-colors.js
@@ -12,15 +12,10 @@
  */
 export const CssColors = Object.freeze({
   LIGHTRED: '#ff2400',
-  DARKRED: '#800000',
   LIGHTGREEN: '#02fa00',
-  DARKGREEN: '##027300',
   LIGHTBLUE: '#0288fe',
   LIGHTYELLOW: '#f2fb00',
-  DARKYELLOW: '#dcda00',
   LIGHTORANGE: '#ffb800',
-  DARKORANGE: '#ffce00',
-  DARKMAGENTA: '#95007b',
   LIGHTPURPLE: '#e300cd',
   PINK: '#FF33BB'
 })
